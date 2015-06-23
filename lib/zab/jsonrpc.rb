b06f11576425982ff4d2ee8f11193f03740d8b35
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-06-23 16:14:20 +0900

    batch

--- lib/zab/jsonrpc.rb
+++ lib/zab/jsonrpc.rb
@@ -9,42 +9,80 @@ class Zab::Jsonrpc
     @uri = uri
   end
 
-  def post(method, params, id: true)
-    uri = URI.parse(@uri)
-    req = Net::HTTP::Post.new(uri.path)
-    req['Content-Type'] = 'application/json-rpc'
-    req.body = JSON.generate(body(method, params, id: id))
+  def post(method, params, notification: false)
+    request(build(method, params, notification: notification))
+  end
 
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = uri.scheme == 'https'
-    resp = http.request(req)
+  # @param [Hash{String => Any}] builded is result of 'build' method.
+  # @return [Any?] return response['result']
+  def request(builded)
+    uri = URI.parse(@uri)
+
+    req  = req_gen(uri, builded)
+    resp = do_req(uri, req)
+
+    # Parse and error handling
     body = JSON.parse(resp.body)
-    case resp
-    when Net::HTTPSuccess
-      raise Error.new(body) if body['error']
-      return body['result']
-    else
-      raise Error.new(body)
+    raise Error.new(body) if body['error']
+
+    return body['result']
+  end
+
+  # XXX: エラー処理はこれでいい?
+  # TODO: notification がある場合、直感に反する返り方をすると思う
+  # @param [Array<Hash>] buildeds is Array of result of 'build' method.
+  # @return [Array<Any|Error>]
+  def batch(buildeds)
+    uri = URI.parse(@uri)
+    req  = req_gen(uri, buildeds)
+    resp = do_req(uri, req)
+    body = JSON.parse(resp.body)
+    body.map do |r|
+      if r['error']
+        Error.new(r)
+      else
+        r['result']
+      end
     end
   end
 
-
-  private
-
-  def body(method, params, id: true, auth: nil)
+  # @param [String] method is json-rpc method name.
+  # @param [Any?] params is json-rpc parameters.
+  # @param [Boolean] notification
+  # @param [String|NilClass] auth is authorization token.
+  def build(method, params, notification: false, auth: nil)
     res = {
       jsonrpc: VERSION,
       method:  method,
       params:  params,
       auth:    auth,
     }
-    res['id'] = id_gen if id
+    res[:id] = id_gen unless notification
 
     return res
   end
 
+
+  private
   def id_gen
     return rand(10**12)
+  end
+
+  def req_gen(uri, body)
+    req = Net::HTTP::Post.new(uri.path)
+    req['Content-Type'] = 'application/json-rpc'
+    req.body = JSON.generate(body)
+    return req
+  end
+
+  def do_req(uri, req)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.scheme == 'https'
+    resp = http.request(req) # TODO: ここでのエラーハンドリング
+    unless resp.is_a? Net::HTTPSuccess
+      raise Error(resp.body)
+    end
+    return resp
   end
 end
 
--- spec/zab/jsonrpc_spec.rb
+++ spec/zab/jsonrpc_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe Zab::Jsonrpc do
+  let(:client){Zab::Jsonrpc.new(ZABBIX_URL)}
+
   describe '#post' do
-    let(:client){Zab::Jsonrpc.new(ZABBIX_URL)}
     let(:params){{user: ZABBIX_USER, password: ZABBIX_PASS}}
     let(:req){client.post('user.login', params)}
 
@@ -15,6 +16,46 @@ describe Zab::Jsonrpc do
 
       it 'should raise error' do
         expect{req}.to raise_error(Zab::Jsonrpc::Error)
+      end
+    end
+  end
+
+  describe '#batch' do
+    let(:buildeds){[
+      client.build('user.login', {user: ZABBIX_USER, password: ZABBIX_PASS}),
+      client.build('user.login', {user: ZABBIX_USER, password: ZABBIX_PASS}),
+    ]}
+
+    let(:batch){client.batch(buildeds)}
+
+    it 'should be Array' do
+      expect(batch).to be_a Array
+
+      expect(batch.size).to eq buildeds.size
+    end
+  end
+
+  describe '#build' do
+    let(:method){'foo'}
+    let(:params){['bar']}
+
+    let(:build){client.build(method, params, notification: notification)}
+
+    context 'when notification' do
+      let(:notification){true}
+
+      it 'should not have id' do
+        expect(build).to be_a Hash
+        expect(build[:id]).to be_nil
+      end
+    end
+
+    context 'when not notification' do
+      let(:notification){false}
+
+      it 'should have id' do
+        expect(build).to be_a Hash
+        expect(build[:id]).not_to be nil
       end
     end
   end
