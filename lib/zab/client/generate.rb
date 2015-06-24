2e22c5c2522304eeac4c02082afd2809306f4ba8
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-06-24 11:39:45 +0900

    Add comments

--- lib/zab/client/generate.rb
+++ lib/zab/client/generate.rb
@@ -5,21 +5,28 @@ methods = JSON.parse(File.read(File.expand_path('../../methods.json', __FILE__))
 methods.each do |name, v|
   class_name = name[0].upcase + name[1..-1] # host => Host
 
+  # Generate Class
   Zab::Client.const_set(class_name, Class.new(Zab::Client) do |klass|
     @class = name
 
     # TODO: getOptions
     v[:methods].each do |method|
+      # Generate query method.
+      # Example: user.login()
       define_method(method) do |params|
         query(method, params)
       end
 
+      # Generate build method. For batch request
+      # Example: user.build_login()
       define_method("build_#{method}") do |params|
         build(method, params)
       end
     end
 
-    Zab::Client.__send__(:define_method, name) do 
+    # Generate getter method.
+    # Example: client.user
+    Zab::Client.__send__(:define_method, name) do
       klass.new(@url, @client)
     end
   end)
