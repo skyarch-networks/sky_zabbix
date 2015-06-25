b2dd26dd904f3aa9e1b3ebae47e396ce31e73897
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-06-25 17:59:36 +0900

    Dynamic versioning.

--- lib/sky_zabbix/version.rb
+++ lib/sky_zabbix/version.rb
@@ -1,3 +1,6 @@
+require 'json'
+
 module SkyZabbix
-  VERSION = "0.1.0"
+  v = JSON.parse(File.read(File.expand_path('../methods.json', __FILE__)), symbolize_names: true)[:version]
+  VERSION = v + ".0.1.0"
 end
