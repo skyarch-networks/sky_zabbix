ab08ef3b2a04e9094502ad06a72de1b17a092a50
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-06-29 13:20:53 +0900

    LIB_VERSION and ZABBIX_VERSION

--- lib/sky_zabbix/version.rb
+++ lib/sky_zabbix/version.rb
@@ -1,6 +1,7 @@
 require 'json'
 
 module SkyZabbix
-  v = JSON.parse(File.read(File.expand_path('../methods.json', __FILE__)), symbolize_names: true)[:version]
-  VERSION = v + ".0.1.0"
+  LIB_VERSION = "0.1.0"
+  ZABBIX_VERSION = JSON.parse(File.read(File.expand_path('../methods.json', __FILE__)), symbolize_names: true)[:version]
+  VERSION = ZABBIX_VERSION + '.' + LIB_VERSION
 end
