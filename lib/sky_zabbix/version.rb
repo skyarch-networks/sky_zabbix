12e40ffb0b6cdf5f1640aad2bd45bb69cc4cefba
candy <lovingrootbeer@gmail.com>
2016-04-08 14:16:52 +0900

    Bump up version to 0.1.2

--- lib/sky_zabbix/version.rb
+++ lib/sky_zabbix/version.rb
@@ -1,7 +1,7 @@
 require 'json'
 
 module SkyZabbix
-  LIB_VERSION = "0.1.1"
+  LIB_VERSION = "0.1.2"
   ZABBIX_VERSION = JSON.parse(File.read(File.expand_path('../methods.json', __FILE__)), symbolize_names: true)[:version]
   VERSION = ZABBIX_VERSION + '.' + LIB_VERSION
 end
